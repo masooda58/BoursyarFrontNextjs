2cd9bdb52accdd001dbf6f97cf21b3e2d50fd288
masoud.a58@gmail.com <masoud.a58@gmail.com>
2022-02-27 19:02:07 +0330

    initial -devextrem+React

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '/node_modules/devextreme/dist/css/dx.material.orange.light.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
